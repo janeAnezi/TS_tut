d1cd398c430789bea6556ab80a2e178d234d67da
Anezijane <ahnnieanezi@gmail.com>
2025-01-21 17:09:00 +0100

    type basics

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+//***********  TYPE BASICS ********** 
 var age = 32;
 var PName = 'ajebo nwa';
 var input = document.querySelectorAll('input');
 console.log(PName);
+var friend = 'angela';
+// friend = 20 type 'number' is not assignable to a type 'string'
 input.forEach(function (input) { return console.log(input); });
+var calc = function (dia) {
+    return dia * Math.PI;
+};
+console.log(calc(8));
+//***********  OBJECTS AND ARRAYS ********** 
--- main.ts
+++ main.ts
@@ -1,7 +1,20 @@
+//***********  TYPE BASICS ********** 
+
 const age = 32;
 const PName = 'ajebo nwa';
 const input = document.querySelectorAll('input');
 console.log(PName);
 
+let friend = 'angela'
+// friend = 20 type 'number' is not assignable to a type 'string'
+
 input.forEach(input => console.log(input));
+
+const calc = (dia: number) => {
+    return dia * Math.PI
+}
+
+console.log(calc(8));
+
+//***********  OBJECTS AND ARRAYS ********** 
 
