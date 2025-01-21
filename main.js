8e7f47bd870100d67dd13c377e8784d112dda20b
Anezijane <ahnnieanezi@gmail.com>
2025-01-21 11:58:16 +0100

    create files

---
+++ main.js
@@ -0,0 +1,5 @@
+var age = 32;
+var PName = 'ajebo nwa';
+var input = document.querySelectorAll('input');
+console.log(PName);
+input.forEach(function (input) { return console.log(input); });
---
+++ main.ts
@@ -0,0 +1,7 @@
+const age = 32;
+const PName = 'ajebo nwa';
+const input = document.querySelectorAll('input');
+console.log(PName);
+
+input.forEach(input => console.log(input));
+
