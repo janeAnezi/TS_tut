c028f17756eb2bc3ee54618c4f565602232ad24f
Anezijane <ahnnieanezi@gmail.com>
2025-01-28 21:35:09 +0100

    functions

--- betterWorkFlow/src/app.ts
+++ betterWorkFlow/src/app.ts
@@ -1,2 +1,15 @@
 console.log('test')
 console.log('test001')
+
+//*********** Functions ********** 
+
+let calcFunc: Function;
+//calcFunc = 23
+calcFunc = () => {
+ console.log('Welcome Again')
+}
+
+let sum = (num1: number, num2: number) => {
+    console.log(num1 + num2);
+}
+sum(5, 7)
