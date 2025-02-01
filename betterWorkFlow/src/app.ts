54dc2b1e631ab8608f83c519c0253355bc949383
Anezijane <ahnnieanezi@gmail.com>
2025-02-01 23:58:54 +0100

    dom and type casting

--- betterWorkFlow/src/app.ts
+++ betterWorkFlow/src/app.ts
@@ -57,3 +57,10 @@ dateOfBrith = (obj: person)=> {
 
 
 //***********   THE DOM AND TYPE CASTING ********** 
+const anchor = document.querySelector('a')!
+
+// if(anchor) {
+//     console.log(anchor.href)
+// }
+ console.log(anchor.href);
+ 
